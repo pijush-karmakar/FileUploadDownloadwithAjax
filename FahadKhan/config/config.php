5fec00ef1a5da17273b2f618b19c510f30e8c568
pijush karmakar <pijushkarmakar45@gmail.com>
2018-10-01 00:13:25 +0600

    file upload with php and Ajax

---
+++ FahadKhan/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "fahad_khan");
+define("TITLE", "Welcome to my site");
+define("AUTHOR", "Pijush");
